84598bae365d8ec4a290c115be70870bf5b5fba4
Mikhail Karasikov <karasikov@users.noreply.github.com>
2026-02-25 19:37:15 +0100

    minor

--- include/sdsl/select_support_mcl.hpp
+++ include/sdsl/select_support_mcl.hpp
@@ -206,9 +206,9 @@ void select_support_mcl<t_b,t_pat_len>::copy(const select_support_mcl<t_b, t_pat
 }
 
 template<uint8_t t_b, uint8_t t_pat_len>
-inline size_type select_support_mcl<t_b,t_pat_len>::miniblock_size(size_type sb_idx) const
+inline auto select_support_mcl<t_b,t_pat_len>::miniblock_size(size_type sb_idx) const -> size_type
 {
-    return static_cast<size_type>((m_miniblock.size() and !m_miniblock[sb_idx]) ? 4096 : 64);
+    return (m_miniblock.size() and !m_miniblock[sb_idx]) ? 4096 : 64;
 }
 
 template<uint8_t t_b, uint8_t t_pat_len>
